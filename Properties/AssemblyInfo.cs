d10c37439bc79a4b4c0f9372e3d7b9ec2b5d2c72
Decider <90727418+xDecider@users.noreply.github.com>
2022-03-06 20:02:27 +0300

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+
+[assembly: AssemblyTitle("TitleChanger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TitleChanger")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("E4221A24-AF92-4855-BE07-5489EF266609")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: MelonInfo(typeof(TitleChanger.AnimatedTitle), "Animated Title", "1.0.0", "Decider", "https://github.com/xDecider")]
+[assembly: MelonColor(ConsoleColor.DarkYellow)]
+[assembly: MelonGame("VRChat", "VRChat")]
